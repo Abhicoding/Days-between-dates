1df7e23bb3f74aa5738cb41f7d230fb260be744e
Abhicoding <shinganeabhishek@gmail.com>
2018-03-05 09:07:14 +0530

    finished pending readme

--- index.js
+++ index.js
@@ -1,18 +1,73 @@
 function solver (string1, string2) {
-  let input1, input2
-  [input1, input2] = [string1, string2].map(inputModify)
-  if (input1[2] < input2[2]) {
-    return daysBetween(input1, input2)
+  let input1, input2, countDays = 0
+  input1 = [string1].map(inputModify)
+  input2 = [string2].map(inputModify)
+  while (!(input1[0] === input2[0] && input1[1] === input2[1] && input1[2] === input2[2])){
+  	countDays++
+  	input1 = incrementor(input1)
   }
-  return daysBetween(input1, input2)
+  return countDays
 }
 
 function inputModify (string) {
   return ([string.substr(0, 2), string.substr(2, 2), string.substr(4)]).map(Number)
 }
 
-console.log(solver('16041991', '12121991'))
+//console.log(daysBetween([16,04,1991], [17,12,1991]))
 
-function daysBetween (array1, array2) {
-	 return [array1, array2]
+/*function daysBetween (array1, array2, count = 0) {
+	 if (compareDay(array1, array2)){
+	 	return incrementDay(array1, array2, count) 
+	 }
+	 if (compareMonth(array1, array2)){
+	 	return incrementMonth(array1, array2, count)
+	 }
+	 if compareYear(array1, array2){
+	 	return incrementYear(array1, array2, count)
+	 }
+}*/
+
+function incrementor(array) {
+	if (array[1] === 12 && array[0] === 31){
+		array = [0, 1, array[2]+1]
+	}
+	else if (array[0] === daysInMonth(array)){
+		array = [0, array[1]+1, array[2]]		
+	}
+	array[0]++
+	return array
 }
+
+function daysInMonth(array){
+	if (array[1] === 2){
+		return (isLeapYear(array)) ? 29 : 28
+	} 
+	if ([4, 6, 9, 11].includes(array[1])){
+		return 30
+	}
+	return 31
+}
+
+function isLeapYear(array) {
+	if(array[2]%400 === 0){
+		return true
+	}
+	if(array[2]%100 === 0){
+		return false
+	}
+	if(array[2]%4 === 0){
+		return true
+	}
+	return false
+}
+
+
+//console.log(solver('31122017', '02012018'))
+//console.log(incrementor([29, 2, 2000]))
+/*function incrementMonth(array1, array2, count) {
+	// body...
+}
+
+function daysInMonth(argument) {
+	// body...
+}*/
